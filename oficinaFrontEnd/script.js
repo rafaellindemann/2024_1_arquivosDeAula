97e50cad65fc0cd848a1419af4a7088fde6bddc4
rafaellindemann <rafaelld@gmail.com>
2024-02-06 10:45:16 -0300

    att oficina frontend

--- oficinaFrontEnd/script.js
+++ oficinaFrontEnd/script.js
@@ -6,3 +6,10 @@ function converterParaReal(){
 
     document.getElementById('saidaReal').innerHTML = 'R$' + real
 }
+function converterParaDolar(){
+    // alert("Depois eu converto, agora estou ocupado")
+    let real = document.getElementById('entradaReal').value
+    let dolar = real / 4.97
+
+    document.getElementById('saidaDolar').innerHTML = 'R$' + dolar.toFixed(2)
+}
