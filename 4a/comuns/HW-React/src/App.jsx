f9354f0d7abfb2b2753ce12752f2cf8509b94e4f
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-02-22 19:57:02 -0300

    Add primeiras aulas de react

---
+++ 4a/comuns/HW-React/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+function apertar(){
+  alert('kashjdf')
+}
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h2>Hello Continent</h2>
+      <button onClick={apertar}>Aperta</button>
+    </>
+  )
+}
+
+export default App
