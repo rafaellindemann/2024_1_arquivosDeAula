e81750b4f3dbfea1954ab542459036bba1784964
rafaellindemann <rafaelld@gmail.com>
2024-03-05 15:59:32 -0300

    Add template e imagens farmárcia

---
+++ 4a/comuns/farmarcia/demoFarmarcia/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 style={{color:'red'}}>Farmárcia</h1>
+      <h2>Sob nova direção</h2>
+    </>
+  )
+}
+
+export default App
