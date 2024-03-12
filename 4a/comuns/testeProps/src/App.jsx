8a4e275723dd8da92a782b5cea73a241d5a1a7d8
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-03-12 18:39:07 -0300

    React props

---
+++ 4a/comuns/testeProps/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Pessoa from './components/Pessoa'
+
+function App() {
+
+  return (
+    <>
+      <Pessoa nome={"Rafael Lindemann"} idade={42}/>
+    </>
+  )
+}
+export default App
+
---
+++ 4a/comuns/testeProps/src/components/Pessoa.jsx
@@ -0,0 +1,13 @@
+
+function Pessoa(props){
+    return(
+        <>
+            <h1>{props.nome}</h1>
+            <h2>{props.idade}</h2>
+        
+        </>
+    )
+  
+}
+
+export default Pessoa;
