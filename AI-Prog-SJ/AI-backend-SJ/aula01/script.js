404c9efa510fdb3e1d23747255ced67668b2584a
Rafael Lindemann Duarte <69912539+rafaellindemann@users.noreply.github.com>
2024-03-25 17:58:41 -0300

    Add arquivos da AI BackEnd aqui porque o git do lab tá ruim

---
+++ AI-Prog-SJ/AI-backend-SJ/aula01/script.js
@@ -0,0 +1,54 @@
+
+// let resposta
+// Estou estudando prompt
+// resposta = prompt('Qual seu nome?')
+
+// alert(resposta)
+// alert('Seu nome é: ' + resposta)
+
+let idade
+idade = 17
+console.log(idade)
+
+idade = idade + 6
+console.log(idade)
+
+// idade = idade + '1'
+// console.log(idade)
+
+idade = idade + 1
+console.log('Somando 1: ' + idade)
+
+idade++
+console.log('Incrementando: ' + idade)
+
+// idade += 5
+
+// subtração
+let idadeNo7a1
+idadeNo7a1 = idade - 10
+console.log('Idade no 7x1: ' + idadeNo7a1);
+
+// multiplicação 
+let dobro
+dobro = idade * 2
+console.log('Dobrado: ' + dobro); 
+
+//console.log('Cada um de vocês me deve R$' + idade); // cueca sendo usada no pé
+
+// divisão
+let metade
+metade = idade / 2
+console.log('Metadinha: ' + metade);
+
+// potência
+let quadrado
+quadrado = idade ** 2
+console.log('Quadrado: ' + quadrado);
+
+
+
+
+
+
+
