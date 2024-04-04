154b4cfcbccad0585e00229193fb5dae4608d609
rafaellindemann <rafaelld@gmail.com>
2024-04-04 11:30:56 -0300

    Testes java #02

--- java/testeFerrugem/src/Main.java
+++ java/testeFerrugem/src/Main.java
@@ -2,15 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.print("Nome do aluno: ");
+//        String nome = teclado.nextLine();
+//        System.out.print("Nota: ");
+//        float nota = teclado.nextFloat();
+//
+//        System.out.printf("A nota de %s é %.3f \n", nome, nota);
+
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Nome do aluno: ");
-        String nome = teclado.nextLine();
-        System.out.print("Nota: ");
-        float nota = teclado.nextFloat();
+        System.out.print("Escreva um int: ");
+        int i = teclado.nextInt();
+        System.out.println("Eco do int: " + i);
 
-        System.out.printf("A nota de %s é %.3f \n", nome, nota);
+        long li = i+1;
+        System.out.println("Int+1 convertido automaticamente pra long na atribuição: " + li);
 
-//        int i = teclado.nextInt();
+        li++;
+        i = (int) li;
+        System.out.println("\nLong incrementado convertido explicitamente para int: " + i);
+
+
+
 
 
     }
