bfb14e4fe0d490191be3383e76780f58caee6b6a
Rafael Lindemann Duarte <69912539+rafaellindemann@users.noreply.github.com>
2024-04-15 22:01:51 -0300

    Ex repetições, parte 1, 3

---
+++ 1aSJ/01-ex-repeticoes/script.js
@@ -0,0 +1,18 @@
+let resultado = document.getElementById("divResultado")
+console.log(resultado.innerHTML);
+resultado.innerHTML = 'mudei'
+// console.log(document.getElementById("divResultado").innerHTML);
+let soma = 0
+let cont = 1
+while(cont<=100){
+    // resultado.innerHTML += cont + "<br>"
+    // soma = soma + cont
+    soma += cont
+    cont++
+}
+resultado.innerHTML = "Resultado da soma: " + soma
+
+n%2 == 1
+n%2 != 0
+
+
