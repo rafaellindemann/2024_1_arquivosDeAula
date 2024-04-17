c857161660257165f1748c5e5381ce774d5f0e69
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-04-16 22:20:44 -0300

    Teste hw java swing

---
+++ java/testeSwing/src/TelaHome.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class TelaHome extends JFrame{
+    static ArrayList<Pessoa> lista = new ArrayList<Pessoa>();
+    public JPanel painelPrincipal;
+    private JTextField txtNome;
+    private JButton btEnviar;
+    private JTextField txtTelefone;
+    private JTextArea txtLista;
+
+    public TelaHome() {
+    btEnviar.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+//            Pessoa p = new Pessoa();
+//            p.setNome(txtNome.getText());
+//            p.setTelefone(txtTelefone.getText());
+
+            Pessoa p = new Pessoa(txtNome.getText(),
+                    txtTelefone.getText());
+
+            lista.add(p);
+
+//            JOptionPane.showMessageDialog(null, lista);
+            txtNome.setText("");
+            txtTelefone.setText("");
+            txtNome.grabFocus();
+            String texto = "";
+            for (Pessoa pessoa : lista){
+                texto += pessoa.mostrarEstado() + "\n---------------\n";
+            }
+            txtLista.setText(texto);
+            System.out.println(texto);
+        }
+    });
+}
+}
