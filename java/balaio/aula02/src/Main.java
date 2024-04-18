8e56e5d409d9261fe385b5faddd90bc38109eb30
rafaellindemann <rafaelld@gmail.com>
2024-04-18 19:02:46 -0300

    Parcial da aula de hoje

---
+++ java/balaio/aula02/src/Main.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.print("Nome: ");
+//        String nome = teclado.nextLine();
+//        System.out.print("Nota: ");
+//        float nota = teclado.nextFloat();
+//
+//        System.out.printf("A nota do aluno %s foi %.1f\n", nome, nota);
+        // criar um projeto pra ler o nome e 3 notas do aluno;
+        // Calcular a média dele
+        // Duvido: dizer se ele passou ou não
+
+
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.print("Nome: ");
+//        String nome = teclado.nextLine();
+//        System.out.print("Primeira Nota: ");
+//        float nota1 = teclado.nextFloat();
+//        System.out.print("Segunda Nota: ");
+//        float nota2 = teclado.nextFloat();
+//        System.out.print("Terceira Nota: ");
+//        float nota3 = teclado.nextFloat();
+//        float media = (nota1+nota2+nota3)/3;
+//
+//        System.out.printf("A media do aluno %s foi %.1f\n", nome, media);
+//        if(media >= 7){
+//            System.out.println("Passou");
+//        }else{
+//            System.out.println("Não foi dessa vez");
+//        }
+
+//        JOptionPane.showMessageDialog(null,"Olá mundo!");
+//        int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade:"));
+//        System.out.println(idade);
+
+        System.out.println(JOptionPane.showConfirmDialog(null, "Tem certeza?"));
+        System.out.println(JOptionPane.showConfirmDialog(null, "Tem certeza?"));
+        System.out.println(JOptionPane.showConfirmDialog(null, "Tem certeza?"));
+        System.out.print("100%");
+
+
+    }
+}
