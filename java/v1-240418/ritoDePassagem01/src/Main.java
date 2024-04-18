e837121b72ff7ddf907db981b13aff32915e6b06
professor <professor@SN-371151.edu.fiesc.com.br>
2024-04-18 17:38:28 -0300

    Parcial do rito de passagem

--- java/v1-240418/ritoDePassagem01/src/Main.java
+++ java/v1-240418/ritoDePassagem01/src/Main.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
-
+import java.util.ArrayList;
 public class Main {
+    static ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
     public static void main(String[] args) {
-
         for(int i =0; i<3; i++){
             String nome =
                 JOptionPane.showInputDialog("Digite o nome da pessoa "+(i+1));
-
+            int idade = Integer.parseInt(JOptionPane.showInputDialog("Idade:"));
+            Pessoa p = new Pessoa(nome, idade);
+            pessoas.add(p);
         }
+        System.out.println(pessoas);
     }
 }
