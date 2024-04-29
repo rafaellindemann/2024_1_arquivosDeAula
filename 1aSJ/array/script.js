df7f9803a9ed0b35a0327147bcb51c8f92e81d53
Rafael Lindemann Duarte <69912539+rafaellindemann@users.noreply.github.com>
2024-04-29 20:24:55 -0300

    Arrays, n1 sj

---
+++ 1aSJ/array/script.js
@@ -0,0 +1,38 @@
+
+// let idade1 = 17
+// let idade2 = 22
+
+// let idades = [17, 22, 23, 19, 44, 15]
+
+// console.log(idades[0]);
+// console.log(idades[1]);
+
+// let i = 0
+// console.log(idades[i]);
+// i++
+// console.log(idades[i]);
+// i++
+// console.log(idades[i]);
+
+
+// console.log(idades);
+
+// let idades = [17, 22, 23, 19, 44, 15, 55, 66, 12, 88]
+
+// for(i=0;i<idades.length;i++){
+//     if(idades[i] >= 0){
+//         console.log(idades[i])
+//     }
+// }
+
+let nomeMeses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+let diasNoMes = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+let n = Number(prompt('Digite o número do mês: '))
+alert("Mês: "+nomeMeses[n-1]+"\nDias: " + diasNoMes[n-1])
+
+// let i = n-1 
+// alert("Mês: "+nomeMeses[i]+"\nDias: " + diasNoMes[i])
+
+
+
