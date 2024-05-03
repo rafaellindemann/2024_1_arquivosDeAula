4181c71bf3bed7b7877835d66f6dbb2f063d8ddc
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-05-03 17:29:48 -0300

    Add teste de dialog com React

---
+++ 4a/comuns/testeDialog/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css'
+function App(){
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openDialog = () => {
+    setIsOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={openDialog}>Abrir diálogo</button>
+      {isOpen && (
+        <dialog open onClose={closeDialog}>
+          <h1>Este é um diálogo</h1>
+          <p>Conteúdo do diálogo...</p>
+          <button onClick={closeDialog}>Fechar</button>
+        </dialog>
+      )}
+    </div>
+  );
+};
+
+export default App;
