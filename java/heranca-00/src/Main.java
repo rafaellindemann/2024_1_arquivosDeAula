2ea2c6b77787c7c82b4f7169bf3271221fe17e5d
rafaellindemann <rafaelld@gmail.com>
2024-06-06 10:29:16 -0300

    Add método para testar classe exata

--- java/heranca-00/src/Main.java
+++ java/heranca-00/src/Main.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 
 public class Main {
+
+    private static boolean testarClasseExata(Object o, Class<?> classe){
+        return o.getClass() == classe;
+    }
+
     public static void main(String[] args) {
         ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
         // Criando um aluno
@@ -15,15 +20,39 @@ public class Main {
         professor.mudarSenha("outraNovaSenha456");
         pessoas.add(professor);
 
+        // Criando uma pessoa
         Pessoa p = new Pessoa("João Pessoa", 111, "1234");
         System.out.println(p.getNome());
         pessoas.add(p);
 
+        // Criando um fornecedor
         Fornecedor f = new Fornecedor("Casa do Vidro", 22, "2233", "Vidros");
         pessoas.add(f);
 
         Estagiario est = new Estagiario("Juca", 17,"1001", "DESI",1700.0f);
         pessoas.add(est);
+
+        System.out.println("====== TESTES DE CLASSE =======");
+        System.out.println("------ ALUNO ------");
+        System.out.print("aluno instanceof Aluno: ");
+        System.out.println(aluno instanceof Aluno);
+        System.out.print("aluno.getClass() == Aluno.class: ");
+        System.out.println(aluno.getClass() == Aluno.class);
+        System.out.print("aluno instanceof Pessoa: ");
+        System.out.println(aluno instanceof Pessoa);
+        System.out.print("aluno.getClass() == Pessoa.class: ");
+
+        Class<?> alunoClass = aluno.getClass();
+        System.out.println(alunoClass == Pessoa.class);
+
+        System.out.println("Testando aluno contra Aluno: " + testarClasseExata(aluno, Aluno.class));
+        System.out.println("Testando aluno contra Pessoa: " + testarClasseExata(aluno, Pessoa.class));
+        System.out.println("Testando est contra Estagiario: " + testarClasseExata(est, Estagiario.class));
+        System.out.println("Testando est contra Aluno: " + testarClasseExata(est, Aluno.class));
+        System.out.println("Testando est contra Pessoa: " + testarClasseExata(est, Pessoa.class));
+
+        System.out.println("------ ALUNO ------");
+        System.out.println("====== TESTES DE CLASSE =======");
 
         for(Pessoa pessoa : pessoas){
             System.out.println("============");
@@ -47,6 +76,9 @@ public class Main {
             System.out.println("----------");
             System.out.println(pessoa.toString());
             System.out.println("----------");
+
+
+
         }
 
     }
