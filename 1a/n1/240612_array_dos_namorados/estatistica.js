1fb290c6f278843e05137aa98de1beb30e22a949
rafaellindemann <rafaelld@gmail.com>
2024-06-12 22:25:11 -0300

    Add aloprações estatísticas

--- 1a/n1/240612_array_dos_namorados/estatistica.js
+++ 1a/n1/240612_array_dos_namorados/estatistica.js
@@ -9,11 +9,29 @@ for(i=0; i<=20; i++){
 for(i=0; i<100000000; i++){
     num = Math.floor(Math.random() * 20)
 
-    contagens[num]++
+    contagens[num]++ // 
+    // contagens[num] = contagens[num] + 1
+}
+console.log(contagens);
+// começar o trampo de mostrar na página
+let maior = 0;
+for(i=0;i<20; i++){
+    if(contagens[i] > maior){
+        maior = contagens[i]
+    }
 }
 
-console.log(contagens);
-
+let container = document.getElementById('container')
+for(i=0; i<=20; i++){
+    // container.innerHTML += i+ ': ' + contagens[i] + '<br>'
+    if(i == contagens.indexOf(maior)){
+        container.innerHTML += `<div class="valorMaior">` + i + ': ' + contagens[i] + '</div>'
+    }else if(i%2==0){
+        container.innerHTML += `<div class="valorPar">` + i + ': ' + contagens[i] + '</div>'
+    }else{
+        container.innerHTML += `<div class="valorImpar">` + i + ': ' + contagens[i] + '</div>'
+    }
+}
 
 
 // 0 .. 19.99999999
