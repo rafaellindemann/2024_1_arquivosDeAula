b449cf82a112b9cc179e8e1b80aeec06156cc265
rafaellindemann <rafaelld@gmail.com>
2024-07-10 21:24:30 -0300

    Add crud completo, SA tá de graça agora

--- 1a/n1/240703-objetos/crud/script.js
+++ 1a/n1/240703-objetos/crud/script.js
@@ -4,6 +4,7 @@ let descricao = document.getElementById('productDescription')
 let preco = document.getElementById('productPrice')
 
 let produtos = []
+let encontrado = -1
 
 function cadastrar(){
     let produto = {
@@ -14,6 +15,10 @@ function cadastrar(){
     produtos.push(produto) 
     console.log(produtos);
 
+    limparFormulario()
+}
+
+function limparFormulario(){
     nome.value = ''
     descricao.value = ''
     preco.value = ''
@@ -23,9 +28,72 @@ function cadastrar(){
 function pesquisar(){
     let pesquisa = nome.value
     for(i=0;i<produtos.length;i++){
-        console.log(produtos[i].nome)
-        // testar se é o certo
+        if(produtos[i].nome == pesquisa){
+            // console.log(produtos[i])
+            descricao.value = produtos[i].descricao
+            preco.value = produtos[i].preco
+            encontrado = i
+        }
     }
-
-    console.log(pesquisa);
 }
+
+function gerarFakes(){
+    let produto
+
+    // 1o produto
+    produto = {
+        nome: 'Kichute',
+        descricao: 'A melhor chuteira não chuteira da história',
+        preco: 123
+    }
+    produtos.push(produto)
+
+    //2o produto
+    produto = {
+        nome: 'Bamba',
+        descricao: 'Eu não lembro como era',
+        preco: 33
+    }
+    produtos.push(produto)
+
+    // 3o produto
+    produto = {
+        nome: 'Caixa de ovo',
+        descricao: 'Linda caixa de papelão com ovos dentro',
+        preco: 12
+    }
+    produtos.push(produto)
+
+    // 4o produto
+    produtos.push({
+        nome: 'Garrafa',
+        descricao: 'Garrafa de tomar no gargalo',
+        preco: 222
+    })
+
+    
+    console.log(produtos);
+
+}
+
+function salvar(){
+    produtos[encontrado].nome = nome.value
+    produtos[encontrado].descricao = descricao.value
+    produtos[encontrado].preco = Number(preco.value)
+
+    limparFormulario()
+    alert("Produto alterado com baita sucesso")
+}
+
+function deletar(){
+    if(encontrado != -1){
+        produtos.splice(encontrado,1);
+    
+        limparFormulario()
+        alert("Produto removido pra nunca mais")
+        encontrado = -1
+    }else{
+        alert('Epaaaaa, pesquisa aí primeiro')
+    }
+}
+
