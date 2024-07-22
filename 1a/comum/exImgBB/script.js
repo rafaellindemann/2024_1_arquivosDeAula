b2b3f432c2a9544539941bef93a287bf571643f0
rafaellindemann <rafaelld@gmail.com>
2024-07-22 16:49:27 -0300

    Add um pouco de carinho na interface

--- 1a/comum/exImgBB/script.js
+++ 1a/comum/exImgBB/script.js
@@ -4,6 +4,9 @@ function trocarImagem(){
     let url = document.getElementById('inpImagem').value
 
     document.getElementById("imgPerfil").setAttribute('src', url)
+
+    document.getElementById('inpImagem').value = ''
+
 }
 
 
